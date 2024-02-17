9fcd86fe122676700a698d44f9d7e93d97e93312
Ganesh23Nayak <ganeshrnayak23@gmail.com>
2024-02-17 17:54:26 +0530

    initial commit

---
+++ dataCollection.py
@@ -0,0 +1,52 @@
+import mediapipe as mp
+import numpy as np
+import cv2
+
+
+def datacollection():
+    holistic=mp.solutions.holistic
+    hands=mp.solutions.hands
+    holis=holistic.Holistic()
+    drawing=mp.solutions.drawing_utils
+
+
+    name = input("Enter the Input Data")
+
+    #video input
+    cap=cv2.VideoCapture(0)
+
+    x=[]
+    data_size = 0
+
+
+    while True:
+        lst=[]
+        #read the contents of the video in frames
+        _,frame=cap.read()
+        frame=cv2.flip(frame,1)  #just flip the pic (not necessary)
+
+        res=holis.process(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+        if res.right_hand_landmarks:
+            for i in res.right_hand_landmarks.landmark:
+                lst.append(i.x-res.right_hand_landmarks.landmark[8].x)
+                lst.append(i.y-res.right_hand_landmarks.landmark[8].y)
+        else:
+            for _ in range(21):
+                lst.extend([0.0,0.0])
+
+        x.append(lst)
+        data_size+=1
+
+
+
+        drawing.draw_landmarks(frame,res.right_hand_landmarks,hands.HAND_CONNECTIONS)  #used to mark the points on the frame
+        cv2.putText(frame, str(data_size), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow("window",frame)  #to show us what is capturing
+        #to stop the cam capturing
+        if cv2.waitKey(1)==27 or data_size>99:
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+    np.save(f"{name}.npy",np.array(x))
+    print(np.array(x).shape)
---
+++ main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from dataCollection import datacollection
+import tkinter.font as font
+
+window=tk.Tk()
+window.title("Welcome")
+window.geometry("450x140")
+
+frame1 = tk.Frame(window)
+
+
+btn_font = font.Font(size=15)
+btn1 = tk.Button(frame1,fg="red", text="add data",command=datacollection, height=5, width=10)
+btn1['font'] = btn_font
+btn1.grid(row=0, column=0, padx=(5,5), pady=(10,10))
+
+btn2 = tk.Button(frame1,fg="orange", text="train",command=" ", height=5, width=10)
+btn2['font'] = btn_font
+btn2.grid(row=0, column=1, padx=(5,5), pady=(10,10))
+
+btn3 = tk.Button(frame1,fg="green", text="run",command=" ", height=5, width=10)
+btn3['font'] = btn_font
+btn3.grid(row=0, column=2, padx=(5,5), pady=(10,10))
+
+frame1.pack()
+window.mainloop()
